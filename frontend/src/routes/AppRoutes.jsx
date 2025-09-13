3e44e5f3d659853752663b628759991407e6092e
surajgharde <gharadesuraj@gmail.com>
2025-09-13 14:29:40 +0530

    add frontend

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+const AppRoutes = () => {
+    return (
+        <div>AppRoutes</div>
+    )
+}
+
+export default AppRoutes
