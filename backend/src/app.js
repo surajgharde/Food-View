60e039019622bafa8f6bde903f68846782a0dc0b
surajgharde <gharadesuraj@gmail.com>
2025-09-30 00:15:45 +0530

    Completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const foodRoutes = require('./routes/food.routes');
+const foodPartnerRoutes = require('./routes/food-partner.routes')
 const cors = require('cors');
-
+ 
 const app = express();
 app.use(cors({
     origin: "http://localhost:5173",
@@ -17,8 +18,8 @@ app.get("/", (req,res) => {
     res.send("Hello World");
 })
 
-
 app.use('/api/auth', authRoutes);
 app.use('/api/food', foodRoutes);
+app.use('/api/food-partner',foodPartnerRoutes)
 
 module.exports = app
